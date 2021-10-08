cef69d80c6b2c23c5b2bfd3200462cfcc835ea13
dev_ElderLab <enderlabo@gmail.com>
2021-10-08 14:06:40 -0500

    Waiting until the task finishes and update UI - done.

--- iOS Bootcamp Challenge/Controllers/ListViewController.swift
+++ iOS Bootcamp Challenge/Controllers/ListViewController.swift
@@ -110,20 +110,24 @@ class ListViewController: UICollectionViewController {
 
     @objc func refresh() {
         shouldShowLoader = true
-
         var pokemons: [Pokemon] = []
+        let dispatch = DispatchGroup()
 
-        // TODO: Wait for all requests to finish before updating the collection view
+        //MARK: - Waiting until the task finishes and update UI
 
-        PokeAPI.shared.get(url: "pokemon?limit=30", onCompletion: { (list: PokemonList?) in
+        PokeAPI.shared.get(url: "pokemon?limit=150", onCompletion: { (list: PokemonList?) in
             guard let list = list else { return }
             list.results.forEach { result in
+                dispatch.enter()
                 PokeAPI.shared.get(url: "/pokemon/\(result.id)/", onCompletion: { (pokemon: Pokemon?) in
                     guard let pokemon = pokemon else { return }
                     pokemons.append(pokemon)
-                    self.pokemons = pokemons
-                    self.didRefresh()
+                    dispatch.leave()
                 })
+            }
+            dispatch.notify(queue: .main){
+                self.pokemons = pokemons
+                self.didRefresh()
             }
         })
     }
